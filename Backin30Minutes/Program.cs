589b5b0b9b2d36c27e6e4cc71a2f41fe82a7d06d
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 10:00:21 +0300

    Add files via upload

---
+++ Backin30Minutes/Program.cs
@@ -0,0 +1,23 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int hour = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+        
+        minutes += 30;
+        if  (minutes >= 60)
+        {
+            hour ++;
+            minutes -= 60;
+        }
+
+        if  (hour == 24)
+        {
+            hour = 0;
+        }
+
+        Console.WriteLine($"{hour}:{minutes:d2}");
+    }
+}
