6e922f2a62db3729ed3e1c477af5f55208fd7a0b
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 10:33:43 +0300

    Add files via upload

---
+++ Login/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace StrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            int attempt = 1;
+            char[] newOne = username.ToCharArray();
+            Array.Reverse(newOne);
+            string pass = new string(newOne);
+            while (attempt < 4)
+            {
+                if (password == pass)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    attempt++;
+
+                    password = Console.ReadLine();
+                }
+            }
+            if (attempt == 4 && password == pass)
+            {
+                Console.WriteLine($"User {username} logged in.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"User {username} blocked!");
+            }
+        }
+    }
+}
+
+
