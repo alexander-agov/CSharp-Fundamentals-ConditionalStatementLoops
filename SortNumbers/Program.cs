a71e742969762400f76a02c8b71eb06827d0908a
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:06:55 +0300

    Add files via upload

---
+++ SortNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SortNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int[] myArr = new int[3] { num1, num2, num3 };
+
+            Array.Sort(myArr);
+            Array.Reverse(myArr);
+
+            foreach (int val in myArr)
+            { 
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
---
+++ StrongNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int numCopy = num;
+            int factorialSum = 0;
+
+            while (numCopy > 0)
+            {
+                int digit = numCopy % 10;
+                numCopy = numCopy / 10;
+
+                int factorial = 1;
+                for (int i = 1; i <= digit; i++)
+                {
+                    factorial *= i;
+                }
+                factorialSum += factorial;
+            }
+            Console.WriteLine(num == factorialSum ? "yes" : "no");
+        }
+    }
+}
