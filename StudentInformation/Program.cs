14f97d895aaa9313e04d93cd58d34a07f20e92d7
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:08:26 +0300

    Add files via upload

---
+++ StudentInformation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace StudentInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            int studentAge = int.Parse(Console.ReadLine());
+            double studentGrade = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {studentName}, Age: {studentAge}, Grade: {studentGrade:f2}");
+        }
+    }
+}
---
+++ SumOfOddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for ( int i = 1; i <= n; i++ ) 
+            {
+                int current = 2 * i - 1;
+                Console.WriteLine(current);
+                sum += current;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
