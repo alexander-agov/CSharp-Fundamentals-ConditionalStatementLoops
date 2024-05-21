34bf39b4e4a8145327e09f4b3c99a6988b86da88
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 11:01:53 +0300

    Add files via upload

---
+++ RageExpenses/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RageExpenses
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            var mousePrice = double.Parse(Console.ReadLine());
+            var keyboardPrice = double.Parse(Console.ReadLine());  
+            var displayPrice = double.Parse(Console.ReadLine());
+            var expenses = 0.0;
+
+            if (lostGames / 12 >= 1)
+            {
+                expenses = lostGames / 2 * headsetPrice + lostGames / 3 * mousePrice
+              + lostGames / 6 * keyboardPrice + lostGames / 12 * displayPrice;
+            } else
+            {
+                expenses = lostGames / 2 * headsetPrice + lostGames / 3 * mousePrice
+                    + lostGames / 6 * keyboardPrice;
+            }
+          
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+        }
+    }
+}
---
+++ ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           var str = Console.ReadLine();
+            var newStr = "";
+
+            for (int i = str.Length-1; i >= 0; i--)
+            {
+                newStr += str[i];
+            }
+            Console.WriteLine(newStr);
+        }
+    }
+}
