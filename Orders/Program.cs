5c9eefe88606bc227224f3cae7713caa1de0eb34
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 10:58:12 +0300

    Add files via upload

---
+++ Orders/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Orders
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var totalPrice = 0.0;
+            var price = 0.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                var pricePerCapsule = double.Parse(Console.ReadLine());
+                var daysInMonth = int.Parse(Console.ReadLine());
+                var capsulesCount = int.Parse(Console.ReadLine());
+                price = ((daysInMonth * capsulesCount) * pricePerCapsule);
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+                totalPrice += price;
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
