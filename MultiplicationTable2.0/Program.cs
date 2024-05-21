c02d87bdcc1d1031eed0b930413269dbebba3463
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 10:53:35 +0300

    Add files via upload

---
+++ MultiplicationTable2.0/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MultiplicationTable2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            do
+            {
+                int product = num * times;
+                Console.WriteLine($"{num} X {times} = {product}");
+                times++;
+            } while (times <= 10);
+        }
+    }
+}
