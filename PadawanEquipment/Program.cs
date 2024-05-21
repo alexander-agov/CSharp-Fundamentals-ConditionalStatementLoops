25d50ff172ea4f80570d9a9deeed0be855289785
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-21 10:59:47 +0300

    Add files via upload

---
+++ PadawanEquipment/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PadawanEquipment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double givenMoney = double.Parse(Console.ReadLine());
+            int studentCount = int.Parse(Console.ReadLine());
+            double lightsabersPrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+            double calculatedPrice = 0.0;
+            int freeBelts = 0;
+
+            if (studentCount > 5) 
+            {
+                freeBelts = studentCount / 6;
+            }
+
+            calculatedPrice = lightsabersPrice * studentCount + 
+                lightsabersPrice *( Math.Ceiling(studentCount * 0.1))
+                + robePrice * studentCount + beltPrice * (studentCount - freeBelts);
+            
+            if (calculatedPrice > givenMoney) 
+            {
+                Console.WriteLine($"John will need {(calculatedPrice-givenMoney):f2}lv more.");
+            } else
+            {
+                Console.WriteLine($"The money is enough - it would cost " +
+                    $"{calculatedPrice:f2}lv.");
+            }
+        }
+    }
+}
